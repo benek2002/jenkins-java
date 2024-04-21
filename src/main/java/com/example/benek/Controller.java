996c5c5fae8c8407179ca5870ca1cd589c457db1
benek2002 <pzabkiewicz@student.agh.edu.pl>
2024-04-22 00:03:44 +0200

    Enable CORS

--- src/main/java/com/example/benek/Controller.java
+++ src/main/java/com/example/benek/Controller.java
@@ -1,33 +1,35 @@
 package com.example.benek;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
     @GetMapping
+    @CrossOrigin
     public String hello(){
         return "Hello User!";
     }
     @GetMapping("/substraction/{a}/{b}")
+    @CrossOrigin
     public Double substraction(@PathVariable Double a, @PathVariable Double b){
         return a - b;
     }
 
     @GetMapping("/addition/{a}/{b}")
+    @CrossOrigin
     public Double addition(@PathVariable Double a, @PathVariable Double b){
         return a + b;
     }
 
     @GetMapping("/multiplication/{a}/{b}")
+    @CrossOrigin
     public Double multiplication(@PathVariable Double a, @PathVariable Double b){
         return a * b;
     }
 
     @GetMapping("/division/{a}/{b}")
+    @CrossOrigin
     public Double division(@PathVariable Double a, @PathVariable Double b){
         return a/b;
     }
